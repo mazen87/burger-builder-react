6505850d42c9f9da198c3045262a5994a625b12d
mazen87 <mazen.karfoul87@hotmail.com>
2021-03-22 12:05:50 +0100

    Adding the buildControls component and BuildControl component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classesCss from './BuildControls.css';
+
+const buildControls = (props) => {
+        return (
+            <div className={classesCss.BuildControls} >
+
+            </div>
+        );
+}
+
+export default buildControls;
