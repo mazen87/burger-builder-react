328a9494eebdbcec0faf8271dd1ed2060e4fc845
mazen87 <mazen.karfoul87@hotmail.com>
2021-03-24 15:22:01 +0100

    reusing the backDrop with sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliaire';
 import classesCss from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+//import BackDrop from '../UI/BackDrop/BackDrop';
 
-const Layout = (props)=> (
-    <Aux>
+class Layout extends Component {
+   
+    state = {
+        showSideDrawer : true
+    }
+
+    closeBackDropSideDrawerHandler = ()=> {
+            this.setState({showSideDrawer:false});
+    }
+
+    render () {
        
-        <Toolbar />
-        <SideDrawer />
-        <main className={classesCss.MainContent}>
-            {props.children} 
-        </main>
-    </Aux>    
-);
 
+        return (
+            <Aux>
+       
+                <Toolbar />
+                <SideDrawer  showSideDrawer={this.state.showSideDrawer} close={this.closeBackDropSideDrawerHandler}/>
+               {/*  <BackDrop  showSummary={this.state.showSideDrawer} summaryHide={this.closeBackDropSideDrawerHandler} /> */}
+                <main className={classesCss.MainContent}>
+                    {this.props.children} 
+                </main>
+            </Aux>    
+        );
+    }
+}
 export default Layout;
--- src/components/UI/BackDrop/BackDrop.js
+++ src/components/UI/BackDrop/BackDrop.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classesCss from './BackDrop.css';
 
 const backDrop = (props) => {
-   return  props.showSummary ? <div className={classesCss.BackDrop}   onClick={props.summaryHide}  ></div> : null
+   return props.showSummary ? <div className={classesCss.BackDrop}  onClick={props.summaryHide}></div> : null
 }
 
 export default backDrop;
