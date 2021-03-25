4a53571b695db39104f8623214b4e0057f252120
mazen87 <mazen.karfoul87@hotmail.com>
2021-03-25 07:35:49 +0100

    changing the folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classesCss from './Modal.css';
 //import OrderSummary from '../../Burger/OrderSummary/OrderSummary';
-import Aux from '../../../hoc/Auxiliaire';
+import Aux from '../../../hoc/Auxiliaire/Auxiliaire';
 import BackDrop from '../BackDrop/BackDrop';
 
-const modal = (props) => (
-    <Aux>
-        <BackDrop  showSummary={props.summaryShow} summaryHide={props.summaryHide} />
-        <div className={classesCss.Modal} 
-            style={{ 
-                transform: props.summaryShow ? 'translateY(0)' : 'translateY(-100vh)' ,
-                opacity: props.summaryShow ? '1' : '0'
-        }} >
-        { /* <OrderSummary ingredients={props.ingredients} />*/ }
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal ;
+    componentDidUpdate(){
+        console.log('[modal] componentDidUpdate !');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+              return nextProps.summaryShow !== this.props.summaryShow;
+    }
+
+    render () {
+      
+        return (
+            <Aux>
+            <BackDrop  showSummary={this.props.summaryShow} summaryHide={this.props.summaryHide} />
+            <div className={classesCss.Modal} 
+                style={{ 
+                    transform: this.props.summaryShow ? 'translateY(0)' : 'translateY(-100vh)' ,
+                    opacity: this.props.summaryShow ? '1' : '0'
+            }} >
+            { /* <OrderSummary ingredients={props.ingredients} />*/ }
+                {this.props.children}
+            </div>
+        </Aux>
+        );
+    }
+} 
+
+export default Modal ;
