10a3f2a148afac951bfd064d193c601f8eedcbd7
mazen87 <mazen.karfoul87@hotmail.com>
2021-04-05 11:56:53 +0200

    order submission and passing data between pages

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classesCss from './ContactData.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -9,20 +11,80 @@ class ContactData extends Component {
         adress : {
             street:'',
             postalCode : ''
+        },
+        showSpinner:false,
+        totalPrice:0.5,
+        orderSaved:false
+    }
+
+    hideMessageOrderSavedHandler = () => {
+        setTimeout(()=>{
+            this.setState({orderSaved:false});
+            this.props.history.push('/');
+        },2200);
+    };
+
+    orderHandler = () => {
+        this.setState({showSpinner: true});
+        const order = {
+            ingredients : this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                adress: {
+                    country:'portugal',
+                    street: 'sopreStreet 44',
+                    zipCode : '43556',
+                },
+                email: 'master@email.com',
+                name: 'Master grosso'
+            },
+            deliveryMethod: 'fastest'
         }
+        axios.post('/orders.json', order)
+        .then(response =>   {
+            this.setState({showSpinner:false ,showSummary:false});
+            console.log(response);
+           if(response){
+            this.setState({orderSaved: true})
+            this.hideMessageOrderSavedHandler();
+          
+           }
+         
+        
+        })
+        .catch(error => this.setState({showSpinner: false ,showSummary:false , overSaved:false}));
     }
 
     render () {
+        let form = (
+            <form>
+            <input type="text" placeholder="your Name" />
+            <input type="email" placeholder="your Email" />
+            <input type="text" placeholder="your Street" />
+            <input type="text" placeholder="Postal code " />
+            <Button btnType="Success" clicked={this.orderHandler}> Order</Button>
+        </form>
+        );
+
+        if(this.state.showSpinner){
+            form = <Spinner />
+        }
+        
+
+        let message =null;
+        if(this.state.orderSaved){
+         message = <div style={{width:'100%',height:'200px', margin:'auto',color:'green',padding: '10px' , 
+                        textAlign:'center', fontWeight:'bold',fontSize:'2em'}}>
+                            order has been registered successfully
+                          </div>
+        }
+
+
         return (
             <div className={classesCss.ContactData}>
                 <h4>Please insert your contact data ! </h4>
-                <form>
-                    <input type="text" placeholder="your Name" />
-                    <input type="email" placeholder="your Email" />
-                    <input type="text" placeholder="your Street" />
-                    <input type="text" placeholder="Postal code " />
-                    <Button btnType="Success"> Order</Button>
-                </form>
+                {message}
+                {form}
             </div>
         );
     }
